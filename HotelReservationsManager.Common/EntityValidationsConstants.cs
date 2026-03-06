cdda29685ed53a9d7e3d2fbb6aa07ed608e9a27d
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-03-06 20:17:00 +0200

    Add project files.

---
+++ HotelReservationsManager.Common/EntityValidationsConstants.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HotelReservationsManager.Common
+{
+    public class EntityValidationsConstants
+    {
+        public class User
+        {
+            public const int UsernameMinLength = 3;
+            public const int UsernameMaxLength = 30;
+
+            public const int PasswordMinLength = 6;
+            public const int PasswordMaxLength = 100;
+
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 50;
+
+            public const int EGNLength = 10;
+
+            public const int PhoneNumberLength = 10;
+            
+            public const int EmailMinLength = 10;
+            public const int EmailMaxLength = 255;
+
+        }
+    }
+}
---
+++ HotelReservationsManager.Data.Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using HotelReservationsManager.Common;
+
+namespace HotelReservationsManager.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public  int Id { get; set; }
+
+        [Required]
+        [MaxLength()]
+        public  string Username { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public  string MiddleName { get; set; }
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required] 
+        public string EGN { get; set; } = null!;
+
+        [Required] 
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required] 
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public  DateTime HireDate { get; set; }
+
+        [Required]
+        public  bool IsActive { get; set; }
+
+        public DateTime?  LeavingDate { get; set; }
+    }
+}
