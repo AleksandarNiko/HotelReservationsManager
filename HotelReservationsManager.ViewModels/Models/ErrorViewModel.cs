7975b5c40952aba1f9919da660b8124d3db81cc2
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-03-11 18:16:33 +0200

    Created ViewModels

---
+++ HotelReservationsManager.ViewModels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelReservationsManager.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HotelReservationsManager.ViewModels/Models/ReservationCreateViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationsManager.ViewModels
+{
+    public class ReservationCreateViewModel
+    {
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public List<int> SelectedClientIds { get; set; } = new List<int>();
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime CheckInDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DateAfter(nameof(CheckInDate), ErrorMessage = "Датата на освобождаване трябва да е след датата на настаняване.")]
+        public DateTime CheckOutDate { get; set; }
+
+        public bool HasBreakfast { get; set; }
+        public bool IsAllInclusive { get; set; }
+    }
+
+    public class DateAfterAttribute : ValidationAttribute
+    {
+        private readonly string _startDatePropertyName;
+        public DateAfterAttribute(string startDatePropertyName) => _startDatePropertyName = startDatePropertyName;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var startDateProperty = validationContext.ObjectType.GetProperty(_startDatePropertyName);
+            var startDate = (DateTime)startDateProperty.GetValue(validationContext.ObjectInstance);
+            var endDate = (DateTime)value;
+
+            return endDate > startDate ? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+    }
+}
